6f5217cad924a8c0ee0214125610116f1aab5921
yorman.carrillo <yorman.carrillo@uptc.edu.co>
2024-05-26 02:45:24 +0000

    Se agrego el ingreso de administradores y tutores

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "email", placeholder: "test@test.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        try {
+          const response = await axios.post(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`,
+            {
+              email: credentials?.email,
+              password: credentials?.password,
+            }
+          );
+
+          console.log(response);
+
+          if (response.data.userData.role === "ADMIN" || response.data.userData.role === "TUTOR") {
+            return response.data;
+          } else {
+            return null;
+          }
+        } catch (error) {
+          console.error("Error al autenticar:", error);
+          throw new Error("Ocurrio un error.");
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      session.user = token;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export { handler as GET, handler as POST };
